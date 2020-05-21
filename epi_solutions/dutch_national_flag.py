5d5a3fe9cbc8e6f7442b803fdf51a94337a44b03
Vijay Bhat <vijaysbhat@gmail.com>
2020-05-21 06:59:26 -0700

    Create dutch_national_flag.py

---
+++ epi_solutions/dutch_national_flag.py
@@ -0,0 +1,84 @@
+'''
+Notes:
+* Initial approach was to use quicksort partition twice, once on array and another time on the part >= pivot. 
+  This didn't work because the partition termination logic should be different in both cases (< pivot vs < =pivot)
+  and having two parititioning functions makes for confusing code.
+* Use the EPI solution instead
+
+Bugs in quicksort partition:
+* Forgot to update i and j after the swap
+* Set j to n instead of n - 1
+* Thought I had misremembered Hoare partitioning but turns out there is another partitioning algorithm (Lomuto)
+
+'''
+
+def swap_elements(arr, i, j):
+    t = arr[i]
+    arr[i] = arr[j]
+    arr[j] = t
+
+def quicksort_partition(arr, k):
+    i = 0
+    n = len(arr)
+    j = n - 1
+    pivot = arr[k]
+    while i < j:
+        # if we break on arr[i] >= pivot, the returned value j is the index starting right of which values are >= pivot
+        while i < n and arr[i] < pivot:
+            i += 1
+        # if we break on arr[j] <= pivot, the returned value j is the index starting left of which values are <= pivot
+        while j >= 0 and arr[j] >= pivot:
+            j -= 1
+        if i < j  and i < n and j >= 0:
+            swap_elements(arr, i, j)
+            i += 1
+            j -= 1
+    return arr, j    
+
+def dutch_national_flag(arr, k):
+    '''
+    Doesn't work. Left here for illustrative purposes
+    '''
+    pivot = arr[k]
+    arr, j = quicksort_partition(arr, k)
+    quicksort_partition(arr[j:], pivot)
+    return arr
+
+def dutch_national_flag_epi(arr, k):
+    n = len(arr)
+    pivot = arr[k]
+    '''
+    Keep the following invariants during partitioning
+    * bottom group: arr[0, smaller -1]
+    * middle group: arr[smaller: equal - 1]
+    * unclassified group: arr[equal: larger]
+    * top group: arr[larger + 1: n - 1]
+    '''
+    smaller = equal = 0
+    larger = n - 1
+    while equal <= larger:
+        if (arr[equal] < pivot):
+            swap_elements(arr, smaller, equal)
+            smaller += 1
+            equal += 1
+        elif arr[equal] == pivot:
+            equal += 1
+        else:
+            swap_elements(arr, equal, larger)
+            larger -= 1
+    return arr
+
+if __name__ == '__main__':
+    test_cases = [
+        ([3,1,4,10,1,3,1,10], 0)
+    ]
+    print('Quicksort Partition')
+    for t in test_cases:
+        print(t, quicksort_partition(t[0].copy(), t[1]))
+
+    print('Dutch National Flag Partition')
+    for t in test_cases:
+        print(t, dutch_national_flag_epi(t[0].copy(), t[1]))
+        print(t, dutch_national_flag(t[0].copy(), t[1]))
+
+        

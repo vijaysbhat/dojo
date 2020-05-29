93895e0bc505a2a0bb7f07440feaf09bc42a4861
Vijay Bhat <vijaysbhat@gmail.com>
2020-05-29 06:45:08 -0700

    Create towers_of_hanoi.py

---
+++ epi_solutions/towers_of_hanoi.py
@@ -0,0 +1,21 @@
+'''
+Solve the sequence of moves needed for towers of Hanoi
+
+Notes:
+* what went right
+    *
+* what went wrong
+    * didn't review recursion base case was returning correctly
+'''
+def towers_of_hanoi(n, from_peg, intermediate_peg, to_peg):
+    if n > 1:
+        towers_of_hanoi(n-1, from_peg, to_peg, intermediate_peg)
+    print('Move {} to peg {}'.format(n, to_peg))
+    if n > 1:
+        towers_of_hanoi(n-1, intermediate_peg, from_peg, to_peg)
+
+if __name__ == '__main__':
+    test_cases = [3, 5]
+    for t in test_cases:
+        print('Number of disks', t)
+        towers_of_hanoi(t, 0, 1, 2)

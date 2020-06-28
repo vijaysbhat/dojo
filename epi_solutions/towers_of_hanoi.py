ab8f5d77024a3ee8eaa1854b64babcdde38414ac
Vijay Bhat <vijaysbhat@gmail.com>
2020-06-28 11:09:48 -0700

    Update towers_of_hanoi.py

--- epi_solutions/towers_of_hanoi.py
+++ epi_solutions/towers_of_hanoi.py
@@ -1,6 +1,5 @@
 '''
 Solve the sequence of moves needed for towers of Hanoi
-
 Notes:
 * what went right
     *
@@ -8,11 +7,11 @@ Notes:
     * didn't review recursion base case was returning correctly
 '''
 def towers_of_hanoi(n, from_peg, intermediate_peg, to_peg):
-    if n > 1:
-        towers_of_hanoi(n-1, from_peg, to_peg, intermediate_peg)
+    if n == 0:
+        return
+    towers_of_hanoi(n-1, from_peg, to_peg, intermediate_peg)
     print('Move {} to peg {}'.format(n, to_peg))
-    if n > 1:
-        towers_of_hanoi(n-1, intermediate_peg, from_peg, to_peg)
+    towers_of_hanoi(n-1, intermediate_peg, from_peg, to_peg)
 
 if __name__ == '__main__':
     test_cases = [3, 5]
